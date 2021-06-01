52f5478960975119eca56cc8591480d2b96376c5
pabloosegat <pablo.h.segat@gmail.com>
2021-06-01 20:00:11 -0300

    Implementação dos arquivos 'cadastroCliente.py' e 'cadastroEmpresa.py', incompletos

--- modelagem_de_negocios/persistencia/codigo/cadastroCliente.py
+++ modelagem_de_negocios/persistencia/codigo/cadastroCliente.py
@@ -1,18 +1,19 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from modelagem_de_negocios.util import bd
 from modelagem_de_negocios.util import pathing
-
-mysql = bd.SQL('Ce5tvx5KvM', 'xq09k27yty', 'Ce5tvx5KvM')
 
 rota = pathing.Path()
 app = Flask(__name__, template_folder=rota.templateWay(), static_folder=rota.staticWay())
 
-@app.route('/cadastrarCliente', methods='POST')
+@app.route('/cadastracliente', methods='POST')
 def cadastraCliente():
     username = request.form['username']
     cep = request.form['cep']
     password = request.form['senha']
 
-    cmd = '''
-    INSERT INTO conta_cliente VALUES (%s, %s, PASSWORD(%s));
-    '''
+    mysql = bd.SQL('Ce5tvx5KvM', 'xq09k27yty', 'Ce5tvx5KvM')
+
+    cmd = 'INSERT INTO conta_cliente VALUES (%s, %s, PASSWORD(%s));'
+    mysql.executar(cmd, [username, cep, password])
+
+    return render_template('teste.html', msg=msg)
