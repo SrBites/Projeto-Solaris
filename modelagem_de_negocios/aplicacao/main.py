94cd587ee38885418c9a39d563bb806fab671733
Diogo <diogobites@gmail.com>
2021-06-04 21:00:18 -0300

    att

--- modelagem_de_negocios/aplicacao/main.py
+++ modelagem_de_negocios/aplicacao/main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from modelagem_de_negocios.util import pathing
+from modelagem_de_negocios.util import bd
 
 rota = pathing.Path()
 app = Flask(__name__, template_folder=rota.templateWay(), static_folder=rota.staticWay())
@@ -12,8 +13,25 @@ def index():
 def login():
     return render_template('login.html')
 
+
 @app.route('/cadastro')
 def cadastro():
     return render_template('cadastro.html')
+
+
+@app.route('/cadastracliente', methods=['POST'])
+def cadastraCliente():
+    username = request.form['username']
+    cep = int(request.form['cep'])
+    password = request.form['senha']
+    print('cheguei')
+
+    mysql = bd.SQL('Ce5tvx5KvM', 'xq09k27yty', 'Ce5tvx5KvM')
+    print('cheguei 2')
+    cmd = 'INSERT INTO tb_conta_cliente(nme_cliente,cep_cliente,senha_cliente) VALUES (%s, %s, %s;'
+    mysql.executar(cmd, [username, cep, password])
+    print('cheguei 3')
+    return render_template('teste.html')
+
 
 app.run()
--- modelagem_de_negocios/persistencia/codigo/cadastroCliente.py
+++ modelagem_de_negocios/persistencia/codigo/cadastroCliente.py
@@ -5,15 +5,18 @@ from modelagem_de_negocios.util import pathing
 rota = pathing.Path()
 app = Flask(__name__, template_folder=rota.templateWay(), static_folder=rota.staticWay())
 
-@app.route('/cadastracliente', methods='POST')
+@app.route('/cadastracliente', methods=['POST'])
 def cadastraCliente():
-    username = request.form['username']
-    cep = request.form['cep']
-    password = request.form['senha']
+    username = str(request.form['username'])
+    cep = int(request.form['cep'])
+    password = str(request.form['senha'])
+    print('cheguei')
 
     mysql = bd.SQL('Ce5tvx5KvM', 'xq09k27yty', 'Ce5tvx5KvM')
-
-    cmd = 'INSERT INTO conta_cliente VALUES (%s, %s, PASSWORD(%s));'
+    print('cheguei 2')
+    cmd = 'INSERT INTO tb_conta_cliente(nme_cliente,cep_cliente,senha_cliente) VALUES (%s, %s, PASSWORD(%s));'
     mysql.executar(cmd, [username, cep, password])
+    print('cheguei 3')
+    return render_template('teste.html')
 
-    return render_template('teste.html', msg=msg)
+app.run()
