0985521d48946b61cdee125dc1f2c01889473152
Robson Monteiro <robson22314@gmail.com>
2021-06-16 18:09:34 -0300

    Ajustes e criação do quem somos

--- modelagem_de_negocios/aplicacao/main.py
+++ modelagem_de_negocios/aplicacao/main.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template
+<<<<<<< HEAD
+from modelagem_de_negocios.persistencia.codigo import cadastro#, login
+=======
 from modelagem_de_negocios.persistencia.codigo import cadastro, login
+>>>>>>> 3020bcd519d8e798a1af6f3b00ff6975a2b3c558
 from modelagem_de_negocios.util import pathing
 
 '''
@@ -16,9 +20,9 @@ app = Flask(__name__, template_folder=rota.templateWay(), static_folder=rota.sta
 def index():
     return render_template('index.html')
 
-@app.route('/ajuda')
-def ajuda():
-    return render_template('ajuda.html')
+@app.route('/quemsomos')
+def quemsomos():
+    return render_template('quemsomos.html')
 
 @app.route('/login')
 def formLogin():
